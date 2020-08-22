b428db367d5e56d2a9e56b3839db3172d3171d83
Lim Nhep <limenhep@gmail.com>
2020-08-22 15:00:46 -0700

    Implement tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,42 @@
+// Functional-Shared Instantiation Pattern -> Requires ._extend({}, methods)
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  // {'value' : input}
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // Should be [] instead of null
 
+  // [{'value': 5}] => children[0].value = 5;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-};
+  let addTree = new Tree(value);
+  this.children.push(addTree);
+};// { 'addChild' : func() }
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  for (var i = 0; i < this.children.length; i++) {
+    let child = this.children[i];
+    if (child.contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+// Complexity: What is the time complexity of the above functions?
+// 1. insertion - 0(1) => constant time complexity
+// 2. search - O(n) => linear time operations based on "n"
+
